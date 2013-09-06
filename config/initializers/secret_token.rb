8c236217268d287654fd3d2a3cbb4fcced8c8449
Michael Bick <mafbick@gmail.com>
2013-09-05 18:34:22 -0700

    Added rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MilkenKnightsLists::Application.config.secret_token = '1b826d19a50f881c346fcdaf102d58f399dc66024dbce6b44e30e3594cf869e232f3e47c7ca6c6b2038ccb2237444d8ce52774c0c01da117d00b49ddc32f38af'
